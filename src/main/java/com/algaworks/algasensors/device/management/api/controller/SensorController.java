80891a727566b5f3140a3391d48e370dc009a840
Gilson Silva <gilsonsilvati@gmail.com>
2025-08-30 09:50:53 -0300

    feat: Creat controller

---
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -0,0 +1,27 @@
+package com.algaworks.algasensors.device.management.api.controller;
+
+import com.algaworks.algasensors.device.management.api.model.SensorInput;
+import com.algaworks.algasensors.device.management.common.IdGenerator;
+import com.algaworks.algasensors.device.management.domain.model.Sensor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/sensors")
+public class SensorController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Sensor create(@RequestBody SensorInput input) {
+        return Sensor.builder()
+                .id(IdGenerator.generateTSID())
+                .name(input.name())
+                .ip(input.ip())
+                .location(input.location())
+                .protocol(input.protocol())
+                .model(input.model())
+                .enabled(Boolean.FALSE)
+                .build();
+    }
+
+}
