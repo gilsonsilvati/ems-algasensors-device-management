c86388043efad76e77fdbb4922b0d723de934eb6
Gilson Silva <gilsonsilvati@gmail.com>
2025-08-30 11:23:48 -0300

    feat: Implementando a persistência com Jakarta Persistence e Spring Data JPA

---
+++ src/main/java/com/algaworks/algasensors/device/management/api/config/jpa/TSIDToLongAttributeConverter.java
@@ -0,0 +1,21 @@
+package com.algaworks.algasensors.device.management.api.config.jpa;
+
+import io.hypersistence.tsid.TSID;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class TSIDToLongAttributeConverter implements AttributeConverter<TSID, Long> {
+
+    @Override
+    public Long convertToDatabaseColumn(TSID attribute) {
+
+        return attribute.toLong();
+    }
+
+    @Override
+    public TSID convertToEntityAttribute(Long dbData) {
+
+        return TSID.from(dbData);
+    }
+}
---
+++ src/main/java/com/algaworks/algasensors/device/management/domain/model/SensorId.java
@@ -0,0 +1,45 @@
+package com.algaworks.algasensors.device.management.domain.model;
+
+import io.hypersistence.tsid.TSID;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SensorId implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -3543828547391037890L;
+
+    private TSID value;
+
+    public SensorId(TSID value) {
+        Objects.requireNonNull(value);
+
+        this.value = value;
+    }
+
+    public SensorId(Long value) {
+        Objects.requireNonNull(value);
+
+        this.value = TSID.from(value);
+    }
+
+    public SensorId(String value) {
+        Objects.requireNonNull(value);
+
+        this.value = TSID.from(value);
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+}
