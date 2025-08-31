e290d93778c69f22c66cd33d16246af33800b81b
Gilson Silva <gilsonsilvati@gmail.com>
2025-08-30 21:00:39 -0300

    feat: Ajustando a representação do recurso REST com classe Model

--- src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -1,6 +1,7 @@
 package com.algaworks.algasensors.device.management.api.controller;
 
 import com.algaworks.algasensors.device.management.api.model.SensorInput;
+import com.algaworks.algasensors.device.management.api.model.SensorOutput;
 import com.algaworks.algasensors.device.management.common.IdGenerator;
 import com.algaworks.algasensors.device.management.domain.model.Sensor;
 import com.algaworks.algasensors.device.management.domain.model.SensorId;
@@ -18,7 +19,7 @@ public class SensorController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Sensor create(@RequestBody SensorInput input) {
+    public SensorOutput create(@RequestBody SensorInput input) {
 
         Sensor sensor = Sensor.builder()
                 .id(new SensorId(IdGenerator.generateTSID()))
@@ -30,7 +31,16 @@ public class SensorController {
                 .enabled(Boolean.FALSE)
                 .build();
 
-        return sensorRepository.saveAndFlush(sensor);
-    }
+        sensor = sensorRepository.saveAndFlush(sensor);
 
+        return new SensorOutput(
+                sensor.getId().getValue(),
+                sensor.getName(),
+                sensor.getIp(),
+                sensor.getLocation(),
+                sensor.getProtocol(),
+                sensor.getModel(),
+                sensor.getEnabled()
+        );
+    }
 }
