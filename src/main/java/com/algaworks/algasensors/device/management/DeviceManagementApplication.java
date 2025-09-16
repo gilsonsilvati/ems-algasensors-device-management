0ebd7bbb87da24d77614f7b4bbcde135ad99aff5
Gilson Silva <gilsonsilvati@gmail.com>
2025-09-15 22:28:36 -0300

    feat: Trocar properties por yml

--- src/main/java/com/algaworks/algasensors/device/management/DeviceManagementApplication.java
+++ src/main/java/com/algaworks/algasensors/device/management/DeviceManagementApplication.java
@@ -2,12 +2,14 @@ package com.algaworks.algasensors.device.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class DeviceManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeviceManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceManagementApplication.class, args);
+    }
 
 }
