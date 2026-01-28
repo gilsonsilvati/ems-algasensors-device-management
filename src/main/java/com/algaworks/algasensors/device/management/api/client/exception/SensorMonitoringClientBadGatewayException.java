29efb25cf61f680454dc572cca6342ee075d92b6
Gilson Silva <gilsonsilvati@gmail.com>
2026-01-28 20:18:31 -0300

    feat: Lidando com erros de timeout

--- src/main/java/com/algaworks/algasensors/device/management/api/client/exception/SensorMonitoringClientBadGatewayException.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/client/exception/SensorMonitoringClientBadGatewayException.java
@@ -1,4 +1,9 @@
 package com.algaworks.algasensors.device.management.api.client.exception;
 
+import java.io.Serial;
+
 public class SensorMonitoringClientBadGatewayException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = -8150986130187101908L;
 }
